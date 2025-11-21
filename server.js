37d6815e02ff5ab66143c13b1c974cc66ed4024d
Foyzur Rahman <fr314@live.mdx.ac.uk>
2025-11-21 19:14:34 +0000

    added images for the rest of lessons

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ app.put('/lessons/:id', async (req, res) => {
     }
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
     connectDB();
